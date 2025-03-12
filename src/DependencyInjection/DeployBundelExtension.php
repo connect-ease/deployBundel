7e75e63f609a0ef5c39556023a03c778a9e3c3e1
francois-xavier-darragon <connect.ease.entreprise@gmail.com>
2025-03-12 15:56:04 +0100

    correction

--- src/DependencyInjection/DeployBundelExtension.php
+++ src/DependencyInjection/DeployBundelExtension.php
@@ -12,6 +12,6 @@ class DeployBundelExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-
+        $loader->load('services.yaml');
     }
 }
--- src/DeployBundel.php
+++ src/DeployBundel.php
@@ -1,19 +1,12 @@
 <?php
 namespace DeployBundel;
 
-use DeployBundel\DependencyInjection\DeployBundelExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
 class DeployBundel extends Bundle
 {
-    // public function getContainerExtension(): ?ExtensionInterface
-    // {
-    //     return new DeployBundelExtension();
-    // }
     public function boot()
     {
-        // Appel de la méthode d'installation au démarrage du bundle
         Installer::install();
     }
 }
