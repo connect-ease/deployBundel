c8c1b1ef2e21dfa557b722449093551ecb75ccf9
francois-xavier-darragon <connect.ease.entreprise@gmail.com>
2025-03-12 10:24:08 +0100

    correction

--- src/DeployBundel.php
+++ src/DeployBundel.php
@@ -7,8 +7,13 @@ use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
 class DeployBundel extends Bundle
 {
-    public function getContainerExtension(): ?ExtensionInterface
+    // public function getContainerExtension(): ?ExtensionInterface
+    // {
+    //     return new DeployBundelExtension();
+    // }
+    public function boot()
     {
-        return new DeployBundelExtension();
+        // Appel de la méthode d'installation au démarrage du bundle
+        Installer::install();
     }
 }
